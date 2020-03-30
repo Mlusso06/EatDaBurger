5e74e840cd102792a33e9479e3c18c28b8b7f016
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-03-29 20:33:26 -0500

    adding burger db, server.js and package.json

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drops the Burger database if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database burgers_db and specified it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table plans.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(255) NOT NULL,
+  devoured BOOLEAN DEFAULT false
+  PRIMARY KEY (id)
+);

f7a4d5ba8709e22efbffd95afd1f5439ddaea4f7
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-03-29 20:55:35 -0500

    adding to the schema.sql and seeds.sql to get things running

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Papi', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Taco Truck', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper with Cheese', false);
